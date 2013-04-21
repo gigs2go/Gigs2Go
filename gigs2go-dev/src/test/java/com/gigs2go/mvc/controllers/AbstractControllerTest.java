e284d9b2a3168abfadc97af2809a39750f4e55f2
tim <gigs2go@gmail.com>
2013-04-21 15:08:34 +0100

    Fixed JUnit tests and removed unwanted files

--- gigs2go-dev/src/test/java/com/gigs2go/mvc/controllers/AbstractControllerTest.java
+++ gigs2go-dev/src/test/java/com/gigs2go/mvc/controllers/AbstractControllerTest.java
@@ -5,8 +5,9 @@ package com.gigs2go.mvc.controllers;
 
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-import org.slf4j.Logger; import org.slf4j.LoggerFactory;
 import org.junit.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -18,7 +19,7 @@ import org.springframework.web.context.WebApplicationContext;
  * 
  */
 @WebAppConfiguration
-@ContextConfiguration( { "classpath*:config/repo.xml", "classpath*:config/mvc.xml" } )
+@ContextConfiguration( { "classpath*:config/repo.xml", "classpath*:config/mvc.xml", "classpath*:config/security.xml" } )
 public class AbstractControllerTest {
     private Logger log = LoggerFactory.getLogger( AbstractControllerTest.class );
 
